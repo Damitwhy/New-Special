4783ba80ff458eeb4536c64e75e25ad1f238271d
Kenneth Cox <damitwhy@gmail.com>
2024-08-01 19:54:26 +0000

    Updated create.html

--- engine/models.py
+++ engine/models.py
@@ -6,7 +6,7 @@ from account.models import User
 
 # Create your models here.
 class Engine(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4 ,editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4 , editable=False)
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     
--- engine/views.py
+++ engine/views.py
@@ -16,21 +16,14 @@ def engine(request):
 @login_required
 def engine_create(request):
     if request.method == 'POST':
-        title = request.POST.get('title')
-        description = request.POST.get('description')
+        title = request.POST.get('title','')
+        description = request.POST.get('description','')
         
-        if not title:
-            return render(request, 'engine/create.html', {'error': 'Title is required.'})
-        
-        elif Engine.objects.filter(title=title, created_by=request.user).exists():
-            return render(request, 'engine/create.html', {'error': 'You have already created a list with this title.'})
+        if title:
+            Engine.objects.create(title=title, description=description, created_by=request.user, created_at=timezone.now())
+
+            return redirect('/engine/engine.html')
         else:
-            Engine.objects.create(
-                title=title,
-                description=description,
-                created_by=request.user,
-                created_at=request.user,
-                        
-            )        
+            return render(request, 'engine/create.html', {'error': 'Title is required'})
             
     return render(request, 'engine/engine.html')
